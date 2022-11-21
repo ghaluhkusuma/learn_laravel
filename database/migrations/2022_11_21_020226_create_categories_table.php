5d7c3dd3a2cc464b1a9a4c6d461a256c2b74e9da
Ghaluh Kusuma <ghaluhkusuma21@gmail.com>
2022-11-21 12:41:13 +0700

    Membuat Database

---
+++ database/migrations/2022_11_21_020226_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            // buat column id -> integer, autoIncrement dan primary
+                $table->id();
+            // buat column name -> string
+                $table->string("name");
+            // buat column created_at dan updated_at
+            $table->timestamps();
+            // untuk fitur softdelete 
+                $table->softDeletes();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
